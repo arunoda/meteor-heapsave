f70ae23575fec5b2827c8f6acc6ae3dcb724159e
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2014-07-27 15:16:09 +0530

    initial version

---
+++ package.js
@@ -0,0 +1,12 @@
+Package.describe({
+  "summary": "Take Heapdumps and save them into S3"
+});
+
+Npm.depends({
+  "knox": "0.9.0"
+});
+
+Package.on_use(function(api) {
+  api.add_files("lib/heapsave.js", "server");
+  api.export("HeapSave", "server");
+});
